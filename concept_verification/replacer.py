345e39bba5891efac36cc6a4a7d2079770416e7c
toby <tong.shu.li@gmail.com>
2015-02-26 17:40:15 -0800

    generalized replacer function

--- concept_verification/replacer.py
+++ concept_verification/replacer.py
@@ -1,16 +1,6 @@
 def replacer(target, substitute, source):
 	"""Look for target in source.
 	If found, replace target with substitute.
-	Returns: (found or not, replaced string)
+	Returns: (times found, replaced string)
 	"""
-
-	found = source.count(target)
-
-	if found == 0: # can't find target
-		return (False, source)
-
-	if found == 1:
-		return (True, source.replace(target, substitute))
-
-#	this should never happen
-	raise Exception("Target is not unique in source!")
+	return (source.count(target), source.replace(target, substitute))
