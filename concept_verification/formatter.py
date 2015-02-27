f13ef93d7b55c16c79a9d4167ca0879274b39215
toby <tong.shu.li@gmail.com>
2015-02-26 19:09:17 -0800

    only highlight one of two key concepts

--- concept_verification/formatter.py
+++ concept_verification/formatter.py
@@ -8,27 +8,29 @@ from file_util import read_file
 from web_util import add_tag
 
 sys.path.append("/home/toby/crowdsourcing/concept_verification/")
-from highlight import highlight
+from highlight_concepts import highlight
 from get_pubmed_abstract import get_abstract_information
 from unicode_to_ascii import convert_unicode_to_ascii
 
-from replacer2 import replacer
+from replacer import replacer
+
+import random
 
 def format_abstract(target, formatted_sentence, title, abstract_chunks):
-	times_found = 0
+	times_sentence_found = 0
 
 	found, result = replacer(target, formatted_sentence, title)
 
-	times_found += found
+	times_sentence_found += found
 	formatted_abstract = add_tag("div", "title", result)
 
 	if abstract_chunks:
 		for chunk in abstract_chunks:
 			found, result = replacer(target, formatted_sentence, chunk)
-			times_found += found
+			times_sentence_found += found
 			formatted_abstract += add_tag("div", "abstract_subsection", result)
 
-#	assert times_found == 1, "We messed up in finding our sentence"
+	assert times_sentence_found == 1, "{0}".format(times_sentence_found)
 	return formatted_abstract
 
 def main():
@@ -38,7 +40,7 @@ def main():
 		temp = ["conf_score", "pred_id", "sentence_id", "pubmed_id"]
 		temp += ["gene_id", "predicate", "disease_cui", "sentence"]
 
-		temp += ["formatted_sentence", "subject_text"]
+		temp += ["formatted_sentence", "key_concept"]
 		temp += ["abstract_title", "formatted_abstract"]
 
 		out.write("{0}\n".format("\t".join(temp)))
@@ -67,10 +69,8 @@ def main():
 
 #			info
 			out.write("{0}".format("\t".join(vals)))
-
 			out.write("\t{0}".format(form_sent))
-
-			out.write("\t{0}".format(sub_text))
+#			out.write("\t{0}".format(sub_text))
 
 #			formatted abstract
 			print "sid", sid
@@ -79,14 +79,19 @@ def main():
 			title, abstract_chunks = get_abstract_information(pmid)
 
 			title = convert_unicode_to_ascii(title)
-
-			out.write("\t{0}".format(title))
-
 			formatted_abstract = format_abstract(sent, form_sent, title, abstract_chunks)
 
-			res, formatted_abstract = replacer(sub_text, add_tag("span", "all_sub_text", sub_text), formatted_abstract)
+			poss_concepts = ["subject", "object"]
 
-#			formatted_abstract = format_abstract(sent, form_sent, title, abstract_chunks)
+			key_concept = random.choice(poss_concepts)
+			key_concept = (sub_text if key_concept == "subject" else obj_text)
+
+			out.write("\t{0}".format(key_concept))
+			out.write("\t{0}".format(title))
+
+
+			res, formatted_abstract = replacer(key_concept, add_tag("span", "key_concept", key_concept), formatted_abstract)
+
 			formatted_abstract = add_tag("div", "formatted_abstract", formatted_abstract,
 				"pid {0} sid {1} pmid {2}".format(pid, sid, pmid))
 
