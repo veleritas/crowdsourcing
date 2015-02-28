9ec341115bba254b26a4860497978eb7b8114a74
toby <tong.shu.li@gmail.com>
2015-02-28 13:31:15 -0800

    evaluates confidence scores of both subject and object

--- concept_verification/pred_conf_ranker.py
+++ concept_verification/pred_conf_ranker.py
@@ -1,39 +1,25 @@
-# last updated 2015-02-25 toby
-
-# ranks all of our predications and the sentences they come from
-# by increasing order of confidence
-
+# last updated 2015-02-28 toby
+"""
+Ranks all predications that we might give to the crowd
+based on the confidence of the concept extracted from
+the source text.
+"""
 from collections import defaultdict
 import sys
 sys.path.append("/home/toby/global_util/")
 from file_util import read_file
+sys.path.append("/home/toby/global_util/semmeddb/")
+from get_concept_source_text import get_concept_source_text
 
-from semmed_info import _query_semmed
-from confidence_calculator import confidence
+from confidence_calculator import confidence_score
 import logging
 
-def get_subject_text(sentence_id, predication_id):
-	print sentence_id, predication_id
-	query = ("SELECT SUBJECT_TEXT "
-		"FROM SENTENCE_PREDICATION "
-		"WHERE SENTENCE_ID = %s "
-		"AND PREDICATION_ID = %s LIMIT 1;")
-
-	sub_text = _query_semmed(query, (sentence_id, predication_id))
-	return sub_text
-
-def main():
-
-	logging.basicConfig(filename="errors.log", level=logging.DEBUG)
-#	read all the predicate information
-
-	print "reading predicate info"
-
+def load_data():
 	cur_triple = ()
 	info = defaultdict(set)
 
-	loc = "/home/toby/crowdsourcing/concept_verification/"
-	for line in read_file(loc + "data/triple_info.txt"):
+	loc = "/home/toby/crowdsourcing/concept_verification/data/"
+	for line in read_file("triple_info.txt", loc):
 		line = line.lstrip('\t')
 		vals = line.split('|')
 		if len(vals) == 3:
@@ -49,37 +35,68 @@ def main():
 			sentence = vals[3]
 			info[cur_triple].add((pid, sid, pmid, sentence))
 
-#	go through and calculate confidence levels
-	print "calculating conf levels"
+	return info
+
+def main():
+	logging.basicConfig(filename = "ranking_errors.log", level = logging.DEBUG)
+
+	print "Reading predicate info"
+	info = load_data()
+
+	print "Calculating confidence levels"
 	ans = []
 	for triple, supporting_data in info.items():
-		gid = triple[0]
+		s_cui = triple[0]
+		o_cui = triple[2]
 		for pid, sid, pmid, sentence in supporting_data:
-			sub_text = get_subject_text(sid, pid)
-			if sub_text == "NO_RESPONSE":
-				logging.warning("no db subject text sid {0} pid {1}".format(sid, pid))
+
+			sub_text, obj_text = get_concept_source_text(sid, pid)
+
+			if sub_text == "NO_RESPONSE" or obj_text == "NO_RESPONSE":
+				logging.error("No entry for sid {0} pid {1} in sentence_predication".
+					format(sid, pid))
 				continue
 
-			if sub_text not in sentence:
-				logging.warning("sub_text not in sentence sid {0} pid {1}".format(sid, pid))
+			if not sub_text or not obj_text:
+				logging.error("Empty text fields for sid {0} pid {1}".format(sid, pid))
 				continue
 
-			if not sub_text:
-				logging.warning("sub_text is empty sid {0} pid {1}".format(sid, pid))
+			if (sub_text not in sentence) or (obj_text not in sentence):
+				logging.error("Text not found in sentence sid {0} pid {1}".format(sid, pid))
 				continue
 
+			sub_conf_score = confidence_score(s_cui, sub_text)
+			obj_conf_score = confidence_score(o_cui, obj_text)
 
-			conf_score = confidence(gid, sub_text)
+			print sub_conf_score, obj_conf_score
 
-			print conf_score
-			ans.append((conf_score, pid, sid, pmid, gid, triple[1], triple[2], sentence))
+			ans.append((sub_conf_score, obj_conf_score,
+				pid, sid, pmid,
+				s_cui, triple[1], o_cui, sub_text, obj_text, sentence))
 
+#	print to file
+	header = ["sub_conf_score", "obj_conf_score",
+		"pred_id", "sent_id", "pubmed_id", "s_cui",
+		"predicate", "o_cui", "sub_text", "obj_text", "sentence"]
+
+	print "len ans", len(ans)
 	ans = sorted(ans, key = lambda x: x[0])
-	with open("sorted_predication_by_conf.txt", "w") as out:
+	with open("predication_by_sub_conf.txt", "w") as out:
+		out.write("{0}\n".format("|".join(header)))
 		for item in ans:
-			score = item[0]
-			vals = item[1:]
-			out.write("{0}|{1}\n".format(score, "|".join(vals)))
+			sub_score = item[0]
+			obj_score = item[1]
+			vals = item[2: ]
+			out.write("{0}|{1}|{2}\n".format(sub_score, obj_score, "|".join(vals)))
+
+	ans = sorted(ans, key = lambda x: x[1])
+	with open("predication_by_obj_conf.txt", "w") as out:
+		out.write("{0}\n".format("|".join(header)))
+		for item in ans:
+			sub_score = item[0]
+			obj_score = item[1]
+			vals = item[2: ]
+			out.write("{0}|{1}|{2}\n".format(sub_score, obj_score, "|".join(vals)))
 
 
 if __name__ == "__main__":
