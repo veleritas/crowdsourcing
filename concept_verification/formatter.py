5d287c3917911014a309271f333060ddd169d0a0
toby <tong.shu.li@gmail.com>
2015-02-28 16:16:14 -0800

    formatter now no longer chooses whether the subject or object is highlighted (that job is the picker's job)

--- concept_verification/formatter.py
+++ concept_verification/formatter.py
@@ -1,22 +1,28 @@
-# last updated 2015-02-24 toby
-
-# ask if the extracted concept mappings are correct
-
+# last updated 2015-02-28 toby
+"""
+Takes the already chosen data for display to CrowdFlower
+and adds the remaining information necessary for upload.
+"""
 import sys
 sys.path.append("/home/toby/global_util/")
 from file_util import read_file
 from web_util import add_tag
 
-sys.path.append("/home/toby/crowdsourcing/concept_verification/")
 from highlight_concepts import highlight
 from get_pubmed_abstract import get_abstract_information
 from unicode_to_ascii import convert_unicode_to_ascii
 
 from replacer import replacer
-
-import random
+from itertools import islice
 
 def format_abstract(target, formatted_sentence, title, abstract_chunks):
+	print "target"
+	print target
+
+
+
+
+
 	times_sentence_found = 0
 
 	found, result = replacer(target, formatted_sentence, title)
@@ -34,70 +40,61 @@ def format_abstract(target, formatted_sentence, title, abstract_chunks):
 	return formatted_abstract
 
 def main():
-	print "starting"
+	header = ["chosen_concept", "sub_conf_score", "obj_conf_score",
+		"pred_id", "sent_id", "pubmed_id",
+		"s_cui", "predicate", "o_cui",
+		"sub_text", "obj_text", "sentence",
+		"key_concept",
+		"formatted_sentence", "abstract_title", "formatted_abstract"]
 
 	with open("final_data.tsv", "w") as out:
-		temp = ["conf_score", "pred_id", "sentence_id", "pubmed_id"]
-		temp += ["gene_id", "predicate", "disease_cui", "sentence"]
+		out.write("{0}\n".format("\t".join(header)))
 
-		temp += ["formatted_sentence", "key_concept"]
-		temp += ["abstract_title", "formatted_abstract"]
-
-		out.write("{0}\n".format("\t".join(temp)))
-
-		print "reading"
-		for line in read_file("chosen_data.txt"):
+		for line in islice(read_file("chosen_data.txt"), 1, None):
 			vals = line.split('|')
+			assert len(vals) == 12
 
-			score = vals[0]
-			pid = vals[1]
-			sid = vals[2]
-			pmid = vals[3]
+			sentence = vals[11]
+			pred_id = vals[3]
+			sent_id = vals[4]
+			pmid = vals[5]
 
-			sub = vals[4]
-			pred = vals[5]
-			obj = vals[6]
-			sent = vals[7]
+			print "pmid {0} sid {1}".format(pmid, sent_id)
 
-			print sent, pid, sid
+			form_sent, s_text, o_text = highlight(sentence, pred_id, sent_id)
 
-			form_sent, sub_text, obj_text = highlight(sent, pid, sid)
-			if not form_sent:
+			assert s_text == vals[9], "text {0} vals[9] {1}".format(s_text, vals[9])
+			assert o_text == vals[10], "text {0} vals[10] {1}".format(o_text, vals[10])
+
+			if not form_sent: # can't highlight
 				continue
 
-			print "all good"
-
-#			info
-			out.write("{0}".format("\t".join(vals)))
-			out.write("\t{0}".format(form_sent))
-#			out.write("\t{0}".format(sub_text))
-
-#			formatted abstract
-			print "sid", sid
-			print "pmid:", pmid
-
 			title, abstract_chunks = get_abstract_information(pmid)
-
 			title = convert_unicode_to_ascii(title)
-			formatted_abstract = format_abstract(sent, form_sent, title, abstract_chunks)
 
-			poss_concepts = ["subject", "object"]
+#			highlight the sentence
+			formatted_abstract = format_abstract(sentence, form_sent,
+				title, abstract_chunks)
 
-			key_concept = random.choice(poss_concepts)
-			key_concept = (sub_text if key_concept == "subject" else obj_text)
+#			highlight the remaining instances of the selected concept
+			key_concept_text = vals[9 + ["sub", "obj"].index(vals[0])]
 
-			out.write("\t{0}".format(key_concept))
-			out.write("\t{0}".format(title))
+			res, formatted_abstract = replacer(key_concept_text,
+				add_tag("span", "key_concept", key_concept_text),
+				formatted_abstract)
 
-
-			res, formatted_abstract = replacer(key_concept, add_tag("span", "key_concept", key_concept), formatted_abstract)
-
+#			provide identifier if we want to toggle visibility
 			formatted_abstract = add_tag("div", "formatted_abstract", formatted_abstract,
-				"pid {0} sid {1} pmid {2}".format(pid, sid, pmid))
+				"pid {0} sid {1} pmid {2}".format(pred_id, sent_id, pmid))
 
+#-------------------------------------------------------------------------------
+			out.write("{0}".format("\t".join(vals)))
+			out.write("\t{0}".format(key_concept_text))
+			out.write("\t{0}".format(form_sent))
+			out.write("\t{0}".format(title))
 			out.write("\t{0}\n".format(formatted_abstract))
 
-	print "done"
+	print "Done"
 
 if __name__ == "__main__":
 	main()
