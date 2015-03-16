ff00755a62ceff92d8753186f6013968438aa075
toby <tong.shu.li@gmail.com>
2015-03-16 11:22:15 -0700

    crowdflower task where the instructions for the 4 relationship types are not clear, and no inclusion of deeper semantics

---
+++ extract_relationships/crowdflower/logic.js
@@ -0,0 +1,29 @@
+# last updated 2015-03-12 toby
+
+# file for all of the javascript logic for crowdflower
+
+
+
+require(['jquery-noconflict', 'bootstrap-modal', 'bootstrap-tooltip', 'bootstrap-popover', 'jquery-cookie'], function($) {
+	Window.implement('$', function(el, nc) {
+		return document.id(el, nc, this.document);
+	});
+
+	var $ = window.jQuery;
+
+	$("#toggle_background").click(function(){
+		$("#background_info").toggle();
+	});
+
+	$("#toggle_gene_def").click(function(){
+		$("#gene_definitions").toggle();
+	});
+
+	$("#toggle_chemical_def").click(function(){
+		$("#chemical_definitions").toggle();
+	});
+
+	$("#toggle_disease_def").click(function(){
+		$("#disease_definitions").toggle();
+	});
+});
